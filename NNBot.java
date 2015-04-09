1f6a7c05c2d54ca8bc636d075f011471c1290e06
yeungalan0 <yeungalan0@gmail.com>
2015-04-09 15:23:29 -0600

    Added NN Bot

---
+++ NNBot.java
@@ -0,0 +1,47 @@
+public class NNBot {
+    
+    //Returns an integer array 
+    public static int[] contour(TetrisBoard board){
+	int[] contour = new int[board.width];
+	int lowestBlock = 0;
+	//Find the heights of all of the columns
+	//note that a Tetrisboard.board is stored as [row][column]
+	for (int c = 0; c<board.width; c++){
+	    height = 0
+	    for(int r = 0; r<t_board.height;r++){
+		if (board[r][c] != 0) {
+		    height++;
+		}
+		else {
+		    contour[col] = height;
+		    break;
+		}
+	    }
+	}
+    }
+
+    //Finds the lowest value in the array and subtracts that value from all other value in the array and returns it
+    public void format(int[] contour) {
+
+
+
+
+
+
+
+	
+    }
+
+    public findLowest() {
+
+
+
+
+
+	
+    }
+
+
+    //LOU WORK BELOW HERE!!!
+    
+}
